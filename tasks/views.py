bfb871e09d6d7a09899c5b62c281a3e496381562
dankos914 <kossenko.daniel@gmail.com>
2020-07-31 20:59:23 +0300

    Project created

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm, UpdateTaskForm
 
-# Create your views here.
 
 def main_view(request):
     tasks = Task.objects.all()
